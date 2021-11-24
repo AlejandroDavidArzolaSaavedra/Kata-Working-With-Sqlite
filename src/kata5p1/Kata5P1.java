b4b75edd11cdc7e6986e9251615fd8e5bbb71bb7
Lab-DIS <Lab-DIS@10.0.2.15>
2021-11-24 20:36:58 +0000

    Kata5P1 version 3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,11 +1,27 @@
 package kata5p1;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import static kata5p1.MailListReader.read;
 
 public class Kata5P1 {
-public static void main(String[] args) {
+public static void main(String[] args) throws FileNotFoundException {
+    Kata5P1 kata = new Kata5P1();
+    String urlEmail = "C:\\Users\\Lab-DIS\\Documents\\NetBeansProjects\\Kata5P1\\email.txt";        
+    List<String> email = read(urlEmail);   
+    kata.metodo(email);
+}
+    public void metodo(List<String>email){
+        for(String mail: email){
+            insert(mail);
+        }
+    }
+    
+    /**
     SelectApp app = new SelectApp();
     app.selectAll();
     String url = "jdbc:sqlite:mail.db";
@@ -19,24 +35,28 @@ public static void main(String[] args) {
     } catch (SQLException e) {
     System.out.println(e.getMessage());
         }
-    }
-
-    private static void connect() {
+        */
+    
+    private Connection connect() {
+        String url = "jdbc:sqlite:mail.db";
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:Kata5.db";
             conn = DriverManager.getConnection(url);
-            System.out.println("Connexión a SQLite establecida");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
+        }
+        return conn;
+    }
+    static int i = 1;
+   public void insert(String email) {
+    String sql = "INSERT INTO direcc_email(direccion) VALUES(?)";
+    try (Connection conn = this.connect();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        pstmt.setString(1, email);
+        i+=1;
+        pstmt.executeUpdate();
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
         }
     }
 }
